ecc64aa52d53e644d4a11b3dde90f7926303eab7
rodmachen <rodmachen@gmail.com>
2015-01-08 11:57:39 -0600

    refactor our comment function

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,20 +1,9 @@
 var main = function() {
 	"use strict";
 
-	$(".comment-input button").on("click", function(event) {
+	var addCommentFromInputBox = function () {
 		var $new_comment;
-        if ($(".comment-input input").val() !== "") {
-            var $new_comment = $("<p>").text($(".comment-input input").val());
-            $new_comment.hide();
-            $(".comments").append($new_comment);
-            $new_comment.fadeIn();
-            $(".comment-input input").val("");
-        }
-	});
-	$(".comment-input input").on("keypress", function (event) {
-    var $new_comment;
 
-    if (event.keyCode === 13) {
         if ($(".comment-input input").val() !== "") {
             var $new_comment = $("<p>").text($(".comment-input input").val());
             $new_comment.hide();
@@ -23,8 +12,16 @@ var main = function() {
             $(".comment-input input").val("");
         }
     }
+
+	$(".comment-input button").on("click", function(event) {
+       addCommentFromInputBox();
 	});
 
+	$(".comment-input input").on("keypress", function (event) {
+	    if (event.keyCode === 13) {
+	       addCommentFromInputBox();
+	    }
+	});
 };
 
 $(document).ready(main);
