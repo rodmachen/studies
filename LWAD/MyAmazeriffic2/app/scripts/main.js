76004faf68895eba6da97d3d572c893433bf7e10
rodmachen <rodmachen@gmail.com>
2015-03-23 16:16:45 -0500

    fix initial js; format button/input

--- LWAD/MyAmazeriffic2/app/scripts/main.js
+++ LWAD/MyAmazeriffic2/app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+var main = function () {
+    "use strict";
+
+    $(".comment-input button").on("click", function (event) {
+        console.log("Hello World!");
+    });
+};
+
+$(document).ready(main);
