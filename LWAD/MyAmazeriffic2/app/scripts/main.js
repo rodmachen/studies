8aa8c1269a88b08876f1559f9005f642516542e0
rodmachen <rodmachen@gmail.com>
2015-03-23 18:58:41 -0500

    add fade

--- LWAD/MyAmazeriffic2/app/scripts/main.js
+++ LWAD/MyAmazeriffic2/app/scripts/main.js
@@ -1,10 +1,27 @@
 var main = function () {
-    "use strict";
+    "use strict"; 
 
-    $(".comment-input button").on("click", function (event) {
-    	var $new_comment = $("<p>").text($(".comment-input input").val());
-    	$(".comments").append($new_comment);
-    });
+			$(".comment-input button").on("click", function (event) {
+				var $new_comment;
+
+    		if ($(".comment-input input").val() !== "") {
+					$new_comment = $("<p>").text($(".comment-input input").val());
+					$(".comments").append($new_comment);
+					$(".comment-input input").val("");
+			}
+	});
+			$(".comment-input input").on("keypress", function (event) {
+	    var $new_comment;
+
+	    if (event.keyCode === 13) {
+	        if ($(".comment-input input").val() !== "") {
+	            var $new_comment = $("<p>").text($(".comment-input input").val());
+	            $new_comment.hide();
+	            $(".comments").append($new_comment);
+	            $new_comment.fadeIn();
+	            $(".comment-input input").val("");
+        }
+    }
+});
 };
-
 $(document).ready(main);
