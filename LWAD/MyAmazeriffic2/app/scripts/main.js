d44ffe61c662156675c78d906c27379ae50de8f2
rodmachen <rodmachen@gmail.com>
2015-03-23 16:29:01 -0500

    initial  functionality

--- LWAD/MyAmazeriffic2/app/scripts/main.js
+++ LWAD/MyAmazeriffic2/app/scripts/main.js
@@ -2,7 +2,8 @@ var main = function () {
     "use strict";
 
     $(".comment-input button").on("click", function (event) {
-        console.log("Hello World!");
+    	var $new_comment = $("<p>").text($(".comment-input input").val());
+    	$(".comments").append($new_comment);
     });
 };
 
