7e43a08c1d77a88f8aaab49d7fb17c95547afe49
rodmachen <rodmachen@gmail.com>
2015-03-23 19:10:12 -0500

    refactor DRY

--- LWAD/MyAmazeriffic2/app/scripts/main.js
+++ LWAD/MyAmazeriffic2/app/scripts/main.js
@@ -1,27 +1,28 @@
 var main = function () {
-    "use strict"; 
+	"use strict";
 
-			$(".comment-input button").on("click", function (event) {
-				var $new_comment;
+	var addCommentFromInputBox = function () {
+		var $new_comment;
 
-    		if ($(".comment-input input").val() !== "") {
-					$new_comment = $("<p>").text($(".comment-input input").val());
-					$(".comments").append($new_comment);
-					$(".comment-input input").val("");
-			}
+		if ($(".comment-input input").val() !== "") {
+			$new_comment = $("<p>").text($(".comment-input input").val());
+			$new_comment.hide();
+			$(".comments").append($new_comment);
+			$new_comment.fadeIn();
+			$(".comment-input input").val("");
+		}
+	}
+
+	$(".comment-input button").on("click", function (event) {
+		addCommentFromInputBox();
 	});
-			$(".comment-input input").on("keypress", function (event) {
-	    var $new_comment;
 
-	    if (event.keyCode === 13) {
-	        if ($(".comment-input input").val() !== "") {
-	            var $new_comment = $("<p>").text($(".comment-input input").val());
-	            $new_comment.hide();
-	            $(".comments").append($new_comment);
-	            $new_comment.fadeIn();
-	            $(".comment-input input").val("");
-        }
-    }
-});
+	$(".comment-input input").on("keypress", function (event) {
+
+		if (event.keyCode === 13) {
+			addCommentFromInputBox();
+		}
+	});
 };
+
 $(document).ready(main);
