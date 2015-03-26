b9a8309d024cbac181a0a2f810441b2867ff1bbd
rodmachen <rodmachen@gmail.com>
2015-03-26 12:46:27 -0500

    essentially finished tag functionality

--- LWAD/MyAmazeriffic2/app/scripts/main.js
+++ LWAD/MyAmazeriffic2/app/scripts/main.js
@@ -6,12 +6,52 @@ var main = function (toDoObjects) {
           // of this toDoObject
           return toDo.description;
     });
+var organizeByTags = function (toDoObjects) {
+    var tags = [];
 
+    // iterate over all toDos
+    toDoObjects.forEach(function (toDo) {
+
+        // iterate over each tag in this toDo
+        toDo.tags.forEach(function (tag) {
+
+            // make sure the tag is not already
+            // in the tag array
+            if (tags.indexOf(tag) === -1) {
+                tags.push(tag);
+            }
+        });
+    });
+
+    console.log(tags);
+
+    var tagObjects = tags.map(function (tag) {
+        // here we find all the to-do objects
+        // that contain that tag
+        var toDosWithTag = [];
+        toDoObjects.forEach(function (toDo) {
+
+            // check to make sure the result
+            // of indexOf is *not* equal to -1
+            if (toDo.tags.indexOf(tag) !== -1) {
+                toDosWithTag.push(toDo.description);
+            }
+        });
+
+        // we map each tag to an object that
+        // contains the name of the tag and an array
+        return { "name": tag, "toDos": toDosWithTag };
+    });
+    console.log(tagObjects);
+};
 	$(".tabs a span").toArray().forEach(function (element) {
 		$(element).on("click", function () {
 			var $element = $(element),
-								$content,
+                $content,
                 $input,
+                $inputLabel,
+                $inputTag,
+                $inputTagLabel,
                 $button,
                 i;
 
@@ -31,29 +71,82 @@ var main = function (toDoObjects) {
 	            $content.append($("<li>").text(todo));
 	        });
         } else if ($element.parent().is(":nth-child(3)")) {
-    // THIS IS THE TAGS TAB CODE
             console.log("the tags tab was clicked!");
+
+            var organizedByTag = [
+                {
+                    "name": "shopping",
+                    "toDos": ["Get groceries"]
+                },
+
+                {
+                    "name": "chores",
+                    "toDos": ["Get groceries", "Take Gracie to the park"]
+                },
+
+                {
+                    "name": "writing",
+                    "toDos": ["Make up some new ToDos", "Finish writing this book"]
+                },
+
+                {
+                    "name": "work",
+                    "toDos": ["Make up some new ToDos", "Prep for Monday's class",
+                              "Answer emails", "Finish writing this book"]
+                },
+
+                {
+                    "name": "teaching",
+                    "toDos": ["Prep for Monday's class"]
+                },
+
+                {
+                    "name": "pets",
+                    "toDos": ["Take Gracie to the park"]
+                }
+            ];
+
+            organizedByTag.forEach(function (tag) {
+                    var $tagName = $("<h3>").text(tag.name),
+                        $content = $("<ul>");
+
+
+                    tag.toDos.forEach(function (description) {
+                        var $li = $("<li>").text(description);
+                        $content.append($li);
+                    });
+
+                    $("main .content").append($tagName);
+                    $("main .content").append($content);
+                });
+
         } else if ($element.parent().is(":nth-child(4)")) {
-                $input = $("<input>"),
+                $input = $("<input>").addClass("description"),
+                $inputLabel = $("<p>").text("Description: "),
+                $inputTag = $("<input>").addClass("tags"),
+                $inputTagLabel = $("<p>").text("Tags: "),
                 $button = $('<button class="button tiny radius">').text("+");
 
-                function addInput() {
-                    if ($input.val() !== "") {
-                        toDos.push($input.val());
-                        $input.val("");
-                    }
-                  }
                 $button.on("click", function () {
-                	addInput()
-                });
+                    var description = $input.val(),
+                        tags = $inputTag.val().split(","); // split on the comma
 
-                $input.on("keypress", function () {
-									if (event.keyCode === 13) {
-										addInput()
-                  }
-                });
+                    toDoObjects.push({"description":description, "tags":tags});
 
-                $content = $("<div>").append($input).append($button);
+                    // update toDos
+                    toDos = toDoObjects.map(function (toDo) {
+                          return toDo.description;
+                    });
+
+                    $input.val("");
+                    $inputTag.val("");
+                });
+                $content = $("<div>").append($inputLabel)
+                                    .append($input)
+                                    .append("<p>")
+                                    .append($inputTagLabel)
+                                    .append($inputTag)
+                                    .append($button);
         }
         $("main .content").append($content);
 
