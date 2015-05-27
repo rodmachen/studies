72ae71fa83a727d306b38005e035bc661f0ab01e
rodmachen <rodmachen@gmail.com>
2015-05-26 20:58:32 -0500

    update

--- nodeschool/learnyounode/jugglingAsync.js
+++ nodeschool/learnyounode/jugglingAsync.js
@@ -1,24 +1,19 @@
 var http = require('http');
 var bl = require('bl');
 
-function getter (url1, url2, url3) {
-	var arr = []
-	http.get(url1
+function getter (url) {
+	http.get(url, function(response) {
 		response.pipe(bl(function (err, data) {
-	  	arr.push(data.toString());
+	  	return data.toString();
 		}))
 		.on('error', console.error);
 	});
-	consol.log(arr)
 }
 
-getter(process.argv[2], process.argv[3], process.argv[4])
+// getter(process.argv[2])
+// getter(process.argv[3])
+// getter(process.argv[4])
 
-
-
-	
-		// .setEncoding('utf8')
-
-		// .on("data", function (data) {
-
-		// })
+// getter(process.argv[2], process.argv[3], process.argv[4])
+// getter(process.argv[3], process.argv[3], process.argv[4])
+// getter(process.argv[4], process.argv[3], process.argv[4])
